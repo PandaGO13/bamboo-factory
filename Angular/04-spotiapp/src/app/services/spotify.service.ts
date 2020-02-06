957b4dea862c2b2af4bdc1d2eab4913fd14b6011
Milagros Orihuela <morihuela@NPG.NET>
2020-02-06 12:20:59 -0500

    Update #PGO002

--- Angular/04-spotiapp/src/app/services/spotify.service.ts
+++ Angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -16,10 +16,6 @@ export class SpotifyService {
       'Authorization': 'Bearer BQBbG3KzCf3wOUUHHtTIgtM98KL_zQWJ-vIi4oEyuXPgXWyqRJjtpZqYr8z6thumP7-Hdbj42e73yvp4514'
     });
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-      .subscribe(data => {
-        console.log(data);
-      });
-
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
   }
 }
