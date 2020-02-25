b02a5a5d8ae5c745ea01921555c6284a41d7a756
Milagros Orihuela <morihuela@NPG.NET>
2020-02-25 17:33:40 -0500

    Update #PGO004

--- Angular/04-spotiapp/src/app/services/spotify.service.ts
+++ Angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -11,26 +11,30 @@ export class SpotifyService {
     console.log('Spotify Service Listo');
   }
 
-  getNewReleases(){
+  getQuery( query: string ){
+
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQCPiJi7kQYABZosCXEMYkyn0XWRMq0pXGjzka4wKVQZ-prsRLDpe7dsU6qP7Kjfzqb7F0KS7EgDdjw788I'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-      .pipe( map( data => {
-        return data['albums'].items;
-      }));
+    return this.http.get(url, { headers });
+
+  }
+
+  getNewReleases(){
+
+    return this.getQuery('browse/new-releases?limit=20')
+              .pipe( map( data => {
+                return data['albums'].items;
+              }));
   }
 
   getArtista(termino: string){
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCPiJi7kQYABZosCXEMYkyn0XWRMq0pXGjzka4wKVQZ-prsRLDpe7dsU6qP7Kjfzqb7F0KS7EgDdjw788I'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&market=US&limit=10`, { headers })
-      .pipe( map( data => data['artists'].items ));
+    return this.getQuery(`search?q=${ termino }&type=artist&market=US&limit=10`)
+              .pipe( map( data => data['artists'].items ));
 
   }
 }
