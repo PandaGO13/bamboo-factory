35033db4ade3b5e73094a606f5520cb90c3210ec
Milagros Orihuela <morihuela@NPG.NET>
2020-02-25 17:15:41 -0500

    Update #PGO003

--- Angular/04-spotiapp/src/app/services/spotify.service.ts
+++ Angular/04-spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,23 @@ export class SpotifyService {
   getNewReleases(){
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBbG3KzCf3wOUUHHtTIgtM98KL_zQWJ-vIi4oEyuXPgXWyqRJjtpZqYr8z6thumP7-Hdbj42e73yvp4514'
+      'Authorization': 'Bearer BQCPiJi7kQYABZosCXEMYkyn0XWRMq0pXGjzka4wKVQZ-prsRLDpe7dsU6qP7Kjfzqb7F0KS7EgDdjw788I'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+      .pipe( map( data => {
+        return data['albums'].items;
+      }));
+  }
+
+  getArtista(termino: string){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCPiJi7kQYABZosCXEMYkyn0XWRMq0pXGjzka4wKVQZ-prsRLDpe7dsU6qP7Kjfzqb7F0KS7EgDdjw788I'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&market=US&limit=10`, { headers })
+      .pipe( map( data => data['artists'].items ));
+
   }
 }
